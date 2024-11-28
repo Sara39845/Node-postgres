09ad5ccbfeec3e032b9856f563ce2b85e9f56eef
Sara <perezsara398@gmail.com>
2024-11-27 21:01:09 -0500

    Commit 3  backend node-postgres

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 
 // Configuración de CORS
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const whitelist = ['http://localhost:8080', 'https://myapp.vercel.app'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
