e0ac8f69ebd3725b0c8d140f45f5c97ab440f5f3
Sara <perezsara398@gmail.com>
2024-12-23 17:32:45 -0500

    Autenticacion con passport

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
-require('dotenv').config();
+const { checkApiKey } = require('./middlewares/auth.handler');
 
-// Importación de middlewares de manejo de errores
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-// Usa el puerto que Vercel proporciona automáticamente o 3001 por defecto
-const port = process.env.PORT || 3001;
-
-// Middleware para parsear el cuerpo de las peticiones como JSON
 app.use(express.json());
 
-// Configuración de CORS
-const whitelist = ['http://localhost:8080', 'https://myapp.vercel.app'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
@@ -24,28 +19,27 @@ const options = {
       callback(new Error('no permitido'));
     }
   }
-};
+}
 app.use(cors(options));
 
-// Rutas básicas de prueba
+require('./utils/auth');
+
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/nueva-ruta', checkApiKey, (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-// Configuración de rutas adicionales mediante routerApi
 routerApi(app);
 
-// Middlewares de manejo de errores
 app.use(logErrors);
 app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-// Exporta el app para Vercel
-module.exports = app;
 
-console.log('Loaded Config:', require('./config/config').config);
+app.listen(port, () => {
+  console.log(`Mi port ${port}`);
+});
